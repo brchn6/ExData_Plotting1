8d0f8a0062608ab23a63c3f138c2f3de4a9cf7b6
barco <bar.cohen@weizmann.ac.il>
2023-11-10 21:55:16 +0200

    legend("topleft", legend = c(Sub_metering_1,Sub_metering_2,Sub_metering_3)

--- code/Plot3.R
+++ code/Plot3.R
@@ -1,11 +1,11 @@
 plot(Sub_metering_1 ~ DateTime, data = df2, type = "l", 
      xlab = "", ylab = "Global Active Power", xaxt = 'n', col = "black")
-plot(Sub_metering_2 ~ DateTime, data = df2, type = "l", 
+points(Sub_metering_2 ~ DateTime, data = df2, type = "l", 
      xlab = "", ylab = "Global Active Power", xaxt = 'n' ,col = "red")
-plot(Sub_metering_3 ~ DateTime, data = df2, type = "l", 
-     xlab = "", ylab = "Global Active Power", xaxt = 'n')
+points(Sub_metering_3 ~ DateTime, data = df2, type = "l", 
+     xlab = "", ylab = "Global Active Power", xaxt = 'n', col ="blue")
 
 
-legend()
+legend("topleft", legend = c(Sub_metering_1,Sub_metering_2,Sub_metering_3)
 # Add the custom x-axis
 custom_axis()
