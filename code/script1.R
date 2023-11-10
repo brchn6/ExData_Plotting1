7b47d5559c16ba439dd9b0084e149721909f5992
barco <bar.cohen@weizmann.ac.il>
2023-11-10 10:00:48 +0200

    we ahave a plot

--- code/script1.R
+++ code/script1.R
@@ -1,14 +1,22 @@
-#import df
-#df <- read.table('Data\\household_power_consumption.txt',sep=';', header=TRUE)
-#re-class the Date col
-as.Date(df$Date, format="%d/%m/%Y")
-#re-class the Time col
-df$Time <- strptime(df$Time, format = "%H:%M:%S")
+library(hms)
+
+# Import df
+df <- read.table('Data\\household_power_consumption.txt', sep = ';', header = TRUE)
+
+# Reclass the Date col on the original df
+df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
+
+# Subsetting the big df into df2 that holds only dates from 01/02/2007 to 02/02/2007
+df2 <- subset(df, Date >= as.Date("01/02/2007", format = "%d/%m/%Y") & Date <= as.Date("02/02/2007", format = "%d/%m/%Y"))
+
+#re-class the Time col using hms
+df2$Time <- as.hms(strptime(df2$Time, format = "%H:%M:%S"))
+
 #re-class the Global_active_power col
-df$Global_active_power <- as.numeric(as.character(df$Global_active_power)) 
+df2$Global_active_power <- as.numeric(as.character(df2$Global_active_power)) 
+
 #disable the scientific way of writing numbers
-options(scipen = 999) 
-#subsetting the big df into df that holds onky dates from the 01/02/2007 - 02/02/2007 # nolint
-df<- subset(df , Date >= as.Date("01/02/2007") & Date <= as.Date("02/02/2007"))
+options(scipen = 999)
+
 #plot the hist plot
-hist(df$Global_active_power, col = "red", xlim= c(0,6), main =" Global Active Power" , xlab= "Global active power (kilowatt)")
+hist(df2$Global_active_power, col = "red", xlim= c(0,6),ylim= c(0,1200), main = " Global Active Power") # nolint
