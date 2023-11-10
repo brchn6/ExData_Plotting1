df11ab8071463df3f1861697eaa0670ba92bf675
barco <bar.cohen@weizmann.ac.il>
2023-11-10 08:43:29 +0200

    scipen

--- code/script1.R
+++ code/script1.R
@@ -1,14 +1,12 @@
 #import df
-df <- read.table('C:\\Users\\barc\\Dropbox (Weizmann Institute)\\MSc_Weizmann\\Learning_stuff\\R_coursera\\Peer-graded Assignment Course Project 1\\exdata_data_household_power_consumption\\household_power_consumption.txt',sep=';', header=TRUE)
+#df <- read.table('Data\\household_power_consumption.txt',sep=';', header=TRUE)
 #re-class the Date col
-df$Date<- as.Date(df$Date)
+as.Date(df$Date, format="%d/%m/%Y")
 #re-class the Time col
-df$Time <- strptime(df$Time, "%H:%M:%S")
+df$Time <- strptime(df$Time, format = "%H:%M:%S")
 #re-class the Global_active_power col
-df$Global_active_power <- as.numeric(df$Global_active_power)
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power)) 
+#disable the scientific way of writing numbers
+options(scipen = 999) 
 #plot the hist plot
-hist(df$Global_active_power,
-col = "red", xlim= c(0,6), 
-main =" Global Active Power", 
-nclass = )
-
+hist(df$Global_active_power, col = "red", xlim= c(0,6), main =" Global Active Power")
