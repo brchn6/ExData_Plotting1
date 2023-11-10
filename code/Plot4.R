4c35bb08238da218197d84d3cba213ddb19969a8
barco <bar.cohen@weizmann.ac.il>
2023-11-10 23:27:02 +0200

    plot 4

---
+++ code/Plot4.R
@@ -0,0 +1,51 @@
+library(png)   # Load the png library
+library(grid)  # Load the grid library
+png("ExData_Plotting1\\pngfiles\\plot4.png")
+x_range <- range(df2$DateTime)
+# Calculate the positions
+beginning <- x_range[1]  # start of the x-axis
+end <- x_range[2]        # end of the x-axis
+middle <- beginning + (end - beginning) / 2  # midpoint of the x-axis
+custom_axis <- function() {
+  axis(1, at = c(beginning, middle, end), labels = c("Thu", "Fri", "Sat"))
+}
+
+
+# Set up a 2x2 plotting area
+par(mfrow = c(2, 2))
+
+# Plot 1: Insert PNG image
+plot(Global_active_power ~ DateTime, data = df2, type = "l", 
+     xlab = "", ylab = "Global Active Power", xaxt = 'n')
+
+# Add the custom x-axis
+custom_axis()
+
+# Plot 2 An empty plot
+plot(Voltage ~ DateTime, data = df2, type = "l", 
+     xlab = "datetime", ylab = "Energy sub metering", xaxt = 'n', col = "black")
+# Add the custom x-axis
+custom_axis()
+
+# Plot 3: Another empty plot
+plot(Sub_metering_1 ~ DateTime, data = df2, type = "l", 
+     xlab = "", ylab = "Energy sub metering", xaxt = 'n', col = "black")
+points(Sub_metering_2 ~ DateTime, data = df2, type = "l", 
+     xlab = "", xaxt = 'n' ,col = "red")
+points(Sub_metering_3 ~ DateTime, data = df2, type = "l", 
+     xlab = "", xaxt = 'n', col ="blue")
+
+
+legend("topright", 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), 
+       lwd = 0, bty = "n", # Set line width to 0 and no box type
+       y.intersp = 1.5)    # Adjust vertical spacing between linescustom_axis()
+
+custom_axis()
+# Plot 4: Yet another empty plot
+plot(Global_reactive_power ~ DateTime, data = df2, type = "l", 
+     xlab = "datetime", ylab = "Energy sub metering", xaxt = 'n', col = "black")
+# Add the custom x-axis
+custom_axis()
+dev.off()
